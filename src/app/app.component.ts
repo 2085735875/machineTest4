fbe6cb9ded1f41c22bf5edd6a6ecd76b06579376
2085735875 <rupeshdesale79@gmail.com>
2024-06-16 16:22:50 +0530

    work on mobile task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { mobileProducts } from './shared/conts/mobile';
+import { Imobile } from './shared/model/mobileInterface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'machineTest4';
+  isNotAvaible !: boolean;
+  mobileArr : Array<Imobile> = mobileProducts;
+  ngOnInit(): void {
+    this.isNotAvaible = false
+  }
 }
